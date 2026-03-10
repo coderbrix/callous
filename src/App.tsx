efc6133a1ded7820c3f00f073b17aa1ab8f7cfb2
rayhankobir-dev <rayhankobir893@gmail.com>
2026-03-10 13:24:11 +0600

    feat: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
